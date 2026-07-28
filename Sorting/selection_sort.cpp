890a3c25630d3a20fda23ef8525413f9ed660c14
kishore-reddy-gayam <kishorereddycse860@gmail.com>
2026-07-28 21:41:54 +0530

    Refactor selection sort into a class structure

--- Sorting/selection_sort.cpp
+++ Sorting/selection_sort.cpp
@@ -1,53 +1,61 @@
 #include <iostream>
 using namespace std;
-void selectionSort(int arr[], int n)
+
+class selectionsort
 {
-    for (int i = 0; i < n - 1; i++)
+public:
+    int size;
+    int arr[100];
+    int i;
+
+    void arrayinput()
     {
-        int minIndex = i;
-        for (int j = i + 1; j < n; j++)
+        cout << "enter the size of array" << endl;
+        cin >> size;
+        cout << "enter the array elements" << endl;
+        for (i = 0; i < size; i++)
         {
-            if (arr[j] < arr[minIndex])
-            {
-                minIndex = j;
-            }
+            cin >> arr[i];
         }
-        int temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
     }
-}
-void printArray(int arr[], int n)
-{
-    for (int i = 0; i < n; i++)
+
+    void printarray()
     {
-        cout << arr[i] << " ";
+        for (i = 0; i < size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-}
+
+    void selectionlogic()
+    {
+        int i, j, minIndex, temp;
+        for (i = 0; i < size - 1; i++)
+        {
+            minIndex = i;
+            for (j = i + 1; j < size; j++)
+            {
+                if (arr[j] < arr[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+            temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+};
 
 int main()
 {
-    int n;
-
-    cout << "Enter the number of elements: ";
-    cin >> n;
-
-    int arr[n];
-
-    cout << "Enter " << n << " elements: ";
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-
-    cout << "\nOriginal Array: ";
-    printArray(arr, n);
-
-    selectionSort(arr, n);
-
-    cout << "Sorted Array: ";
-    printArray(arr, n);
-
+    selectionsort ss;
+    ss.arrayinput();
+    cout << "before sorting" << endl;
+    ss.printarray();
+    cout << "after sorting" << endl;
+    ss.selectionlogic();
+    ss.printarray();
     return 0;
 }
